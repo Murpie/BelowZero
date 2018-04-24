870ed304867553184b9fb029a92a3cc974e1c322
LUCAS PERSSON <lupb16@student.bth.se>
2018-04-24 15:45:50 +0200

    Updated mainMenu

--- Game Engine/MainMenuScene.cpp
+++ Game Engine/MainMenuScene.cpp
@@ -122,10 +122,11 @@ void MainMenuScene::loadBackgroundTexture(std::string backgroundTextureName)
 void MainMenuScene::loadBuffers()
 {
 	float vertices[] = {
-		0.5f,  0.5f, 0.0f,  
-		0.5f, -0.5f, 0.0f, 
-		-0.5f, -0.5f, 0.0f,
-		-0.5f,  0.5f, 0.0f 
+		// positions          // colors           // texture coords
+		0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+		0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,  0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,  1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
 	};
 	unsigned int indices[] = { 
 		0, 1, 3,   // first triangle
@@ -143,8 +144,15 @@ void MainMenuScene::loadBuffers()
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+	// color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	// texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
 }
@@ -209,7 +217,13 @@ void MainMenuScene::createShaders()
 
 void MainMenuScene::renderButtons()
 {
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
+
 	glUseProgram(mainMenuShaderProgram);
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, startButtonTexture);
+
 	glBindVertexArray(VAO);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 }
