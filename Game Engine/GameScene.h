8f8264c9ee7fe3ec32e0d1842f65645c8a899cb8
Emil Gustafsson <emiltgustafsson@hotmail.com>
2018-04-29 11:36:52 +0200

    cleaning

--- Game Engine/GameScene.h
+++ Game Engine/GameScene.h
@@ -18,18 +18,20 @@ public:
 	std::vector<GameObject*> gameObjects; 
 
 	void clearGameObjects();
-	void addLight(glm::vec3 transform, int lightType);
-	void addPlayer(); 
-	void addLevelObjects(MeshLib& meshLibrary, MaterialLib& matertialLibrary, LeapLevel* level);
-	void addTerrain(const std::string & heightMap, GLuint shader);
-
 	void update(float deltaTime, float seconds); // Updates all the objects in the scene
 	void processEvents(GLFWwindow *window, float deltaTime); // Updates objects if a evenet occurs, mouse press, button press etc. 
-	void interactionTest(GameObject & other, GLFWwindow * window); // Intersection test on mouse click
 
 	void addLevelScene(MeshLib& meshLibrary, MaterialLib& matertialLibrary, ShaderProgramLib& shader);
 
 private:
 	int camerasInScene; //player 
 	int lightsInScene; 
+
+	void addLight(glm::vec3 transform, int lightType);
+	void addPlayer();
+	void addLevelObjects(MeshLib& meshLibrary, MaterialLib& matertialLibrary, LeapLevel* level);
+	void addTerrain(const std::string & heightMap, GLuint shader);
+
+	void interactionTest(GameObject & other, GLFWwindow * window); // Intersection test on mouse click
+
 };
